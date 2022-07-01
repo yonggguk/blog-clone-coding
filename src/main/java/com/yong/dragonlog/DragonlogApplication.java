66278b260a5cbff32ddfcb54babd7e700f2485c2
yonggguk <yonguk929@gmail.com>
2022-07-02 08:10:48 +0900

    init setting files

---
+++ src/main/java/com/yong/dragonlog/DragonlogApplication.java
@@ -0,0 +1,13 @@
+package com.yong.dragonlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DragonlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DragonlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yong/dragonlog/DragonlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yong.dragonlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DragonlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
