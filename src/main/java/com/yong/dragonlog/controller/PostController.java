fb3a1dfa00c128f26980dbd267a507e3660c25af
yonggguk <yonguk929@gmail.com>
2022-07-11 23:31:09 +0900

    edit PostController.java

--- src/main/java/com/yong/dragonlog/controller/PostController.java
+++ src/main/java/com/yong/dragonlog/controller/PostController.java
@@ -1,2 +1,27 @@
-package com.yong.dragonlog.controller;public class PostController {
+package com.yong.dragonlog.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class PostController {
+
+    // Http Method
+    // GET, POST, PUT, PATCH, DELETE, OPTIONS, HEAD, TRACE, CONNECT
+
+    // SSR -> jsp, thymeleaf, mustache, freemarker
+      // -> html rendering
+
+    // SPA ->
+    //  vue -> vue + SSR = nuxt
+    //  react -> react + SSR = next
+      // -> javascript + <-> API
+    @PostMapping("/posts")
+    public String post(@RequestParam String title, @RequestParam String content){
+        log.info("title={}, content={}", title, content);
+        return "Hello World";
+    }
 }
--- src/test/java/com/yong/dragonlog/controller/PostControllerTest.java
+++ src/test/java/com/yong/dragonlog/controller/PostControllerTest.java
@@ -1,4 +1,25 @@
+package com.yong.dragonlog.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@WebMvcTest
 class PostControllerTest {
-  
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("/posts 시 제목 내용을 출력한다")
+    void test(){
+
+    }
+
 }
